bff76c71702262ffea020f49dcd884283c03fd0f
Matt Thomson <red.cataclysm@gmail.com>
2017-08-24 17:53:48 -0400

    Win Detection

--- public/js/ttt.js
+++ public/js/ttt.js
@@ -4,6 +4,7 @@ var cells = document.querySelectorAll('.cell');
 var gameId = document.getElementById('game').dataset.gameId;
 var me = document.getElementById('game').dataset.player;
 var yourTurn;
+var completed;
 
 socket.emit('load', { gameId: gameId });
 
@@ -11,7 +12,7 @@ for (var cell of cells) {
   cell.addEventListener('click', (e) => {
     var index = e.currentTarget.dataset.cell;
 
-    if (!game[index] && yourTurn) {
+    if (!game[index] && yourTurn && !completed) {
       socket.emit('move', { gameId: gameId, index: index });
     }
 
@@ -23,8 +24,13 @@ socket.on('update', (data) => {
   if (data.gameId === gameId) {
     yourTurn = data.player === me;
     game = data.game;
+    completed = data.completed
     for (let i = 0; i < cells.length; i++) {
       cells[i].innerText = game[i];
+    }
+
+    if (completed) {
+      document.getElementById('title').innerText = `${completed} Wins!`;
     }
   }
 });
--- server/index.js
+++ server/index.js
@@ -23,6 +23,7 @@ app.get('/:gameId/:player', (req, res) => {
 
 let games = {};
 let players = {};
+let completed = {};
 
 io.on('connection', (socket) => {
   console.log('New connection');
@@ -31,21 +32,48 @@ io.on('connection', (socket) => {
     if (!games[data.gameId]) {
       games[data.gameId] = [null, null, null, null, null, null, null, null, null];
       players[data.gameId] = true;
+      completed[data.gameId] = false;
     }
-    socket.emit('update', { gameId: data.gameId, game: games[data.gameId], player: players[data.gameId] ? 'O' : 'X' });
+    socket.emit('update', { 
+      gameId: data.gameId, 
+      game: games[data.gameId],
+      completed: completed[data.gameId],
+      player: players[data.gameId] ? 'O' : 'X'
+    });
   });
 
   socket.on('move', (data) => {
     console.log(`Move: ${data.gameId} - ${data.index}`);
-    games[data.gameId][data.index] = players[data.gameId] ? 'O' : 'X';
-    players[data.gameId] = !players[data.gameId];
-    io.emit('update', { gameId: data.gameId, game: games[data.gameId], player: players[data.gameId] ? 'O' : 'X' });
+    if (!completed[data.gameId]) {
+      games[data.gameId][data.index] = players[data.gameId] ? 'O' : 'X';
+      if(isCompleted(games[data.gameId])) {
+        completed[data.gameId] = players[data.gameId] ? 'O' : 'X';
+      }
+      players[data.gameId] = !players[data.gameId];
+      io.emit('update', { 
+        gameId: data.gameId, 
+        game: games[data.gameId], 
+        completed: completed[data.gameId],
+        player: players[data.gameId] ? 'O' : 'X' 
+      });
+    }
   });
 
   socket.on('disconnect', () => {
     console.log('Disconnected');
   });
 });
+
+function isCompleted(game) {
+  return (game[0] === game[1] && game[1] === game[2] && game[2] !== null) || 
+    (game[3] === game[4] && game[4] === game[5] && game[5] !== null) ||
+    (game[6] === game[7] && game[7] === game[8] && game[8] !== null) ||
+    (game[0] === game[3] && game[3] === game[6] && game[6] !== null) || 
+    (game[1] === game[4] && game[4] === game[7] && game[7] !== null) ||
+    (game[2] === game[5] && game[5] === game[8] && game[8] !== null) ||
+    (game[0] === game[4] && game[4] === game[8] && game[8] !== null) ||
+    (game[2] === game[4] && game[4] === game[6] && game[6] !== null);
+}
 
 server.listen(appPort, () => {
   console.log(`Server listening on ${appPort}.`);
